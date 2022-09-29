841686be1c7a09cc1d818e96b45c561b30d8282c
Pablo Alfonso Rodriguez Rodriguez <pabloalfonsorodriguez2191998@outlook.com>
2022-09-29 00:58:28 -0500

    ChatJS

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+
+import 'stream-chat-react/dist/';
+import './App.css';
+
+const cookies = new Cookies();
+
+const apikey = 'wv755v36pd2n';
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apikey);
+const App = () => {
+    const [createType, setCreateType] = useState('');
+    const [isCreating, setIsCreating] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+
+    if(!authToken) return <Auth />
+
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    setCreateType={setCreateType}
+                    setIsEditing={setIsEditing}
+                />
+                <ChannelContainer 
+                    isCreating={isCreating}
+                    setIsCreating={setIsCreating}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    createType={createType}
+                />
+            </Chat>
+        </div>
+    );
+}
+
+export default App;
